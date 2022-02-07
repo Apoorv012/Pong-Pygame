d5b0b39bd7e6dd66fb822e3e8bbd3191cd0763ac
Apoorv012 <apoorvmitthu@gmail.com>
2022-02-07 23:25:48 +0530

    Reorganized code and made into classes

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # CONSTANTS
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1152
+SCREEN_HEIGHT = 648
 PLAYER_WIDTH = 10
 PLAYER_HEIGHT = 140
 FPS = 60
@@ -25,39 +25,27 @@ FONT_SIZE = 32
 GAME_FONT = pygame.font.Font("freesansbold.ttf", FONT_SIZE)
 PAUSE_FONT = pygame.font.Font("freesansbold.ttf", FONT_SIZE * 2)
 
-# Setting up the main window
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Pong Game")
-
-# Game Rectangles
-ball = pygame.Rect(SCREEN_WIDTH / 2 - BALL_SIZE / 2, SCREEN_HEIGHT / 2 - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE)
-player = pygame.Rect(10 , SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
-opponent = pygame.Rect(SCREEN_WIDTH - PLAYER_WIDTH - 10, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
-
 # Game variables
-ball_speed_x = 7 * random.choice([-1, 1])
-ball_speed_y = 7 * random.choice([-1, 1])
-player_speed = 0
-player_score = 0
-opponent_score = 0
+scoreboard = {
+    "player": 0,
+    "opponent": 0
+}
 isPaused = True
 stopTick = pygame.time.get_ticks()
 currentTick = pygame.time.get_ticks()
 
 ## Functions
 def draw():
-    global SCREEN_WIDTH, SCREEN_HEIGHT, player_score, opponent_score, isPaused, stopTick, currentTick
-
     screen.fill(BG_COLOR)
     pygame.draw.rect(screen, LIGHT_GREY, player)
     pygame.draw.rect(screen, LIGHT_GREY, opponent)
-    pygame.draw.ellipse(screen, LIGHT_GREY, ball)
+    pygame.draw.ellipse(screen, LIGHT_GREY, ball.rect)
     pygame.draw.aaline(screen, LIGHT_GREY, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
 
-    player_score_text = GAME_FONT.render(f"{player_score}", False, LIGHT_GREY)
+    player_score_text = GAME_FONT.render(str(scoreboard["player"]), False, LIGHT_GREY)
     screen.blit(player_score_text, (SCREEN_WIDTH / 2 - player_score_text.get_width() / 2 - 40, SCREEN_HEIGHT / 2 - FONT_SIZE / 2))
 
-    opponent_score_text = GAME_FONT.render(f"{opponent_score}", False, LIGHT_GREY)
+    opponent_score_text = GAME_FONT.render(str(scoreboard["opponent"]), False, LIGHT_GREY)
     screen.blit(opponent_score_text, (SCREEN_WIDTH / 2 - opponent_score_text.get_width() / 2 + 40, SCREEN_HEIGHT / 2 - FONT_SIZE / 2))
 
     if isPaused:
@@ -66,62 +54,98 @@ def draw():
         screen.blit(pause_text, (SCREEN_WIDTH / 2 - pause_text.get_width() / 2, SCREEN_HEIGHT / 2 - FONT_SIZE / 2 - 100))
 
     pygame.display.update()
-
-def ball_animation():
-    global ball_speed_x, ball_speed_y, opponent_score, player_score, isPaused, stopTick
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
-        ball_speed_y *= -1
-    if ball.left <= 0:
-        opponent_score += 1
-        stopTick = pygame.time.get_ticks()
-        isPaused = True
-    if ball.right >= SCREEN_WIDTH:
-        player_score += 1
-        stopTick = pygame.time.get_ticks()
-        isPaused = True
-    
-    if ball.colliderect(player):
-        if abs(ball.left - player.right) < 10:
-            ball_speed_x *= -1
-        else:
-            ball_speed_y *= -1
-    
-    if ball.colliderect(opponent):
-        if abs(ball.right - opponent.left) < 10:
-            ball_speed_x *= -1
-
-def player_animation():
-    player.y += player_speed
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= SCREEN_HEIGHT:
-        player.bottom = SCREEN_HEIGHT
-
-def opponent_animation():
-    if opponent.top < ball.top:
-        opponent.top += OPPONENT_SPEED
-    if opponent.bottom > ball.bottom:
-        opponent.bottom -= OPPONENT_SPEED
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= SCREEN_HEIGHT:
-        opponent.bottom = SCREEN_HEIGHT
-
-def ball_restart():
-    global ball_speed_x, ball_speed_y
-    ball.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    ball_speed_x *= random.choice([-1, 1])
-    ball_speed_y *= random.choice([-1, 1])
+       
 
 def countdown_timer():
     global isPaused, stopTick, currentTick
     currentTick = pygame.time.get_ticks()
     if currentTick - stopTick > 3000:
-        ball_restart()
+        ball.restart()
         isPaused = False
+
+# Setting up the main window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pong Game")
+
+## Classes
+
+class Ball:
+    def __init__(self, scoreboard):
+        self.rect = pygame.Rect(SCREEN_WIDTH / 2 - BALL_SIZE / 2, SCREEN_HEIGHT / 2 - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE)
+        self.speedX = 7 * random.choice([-1, 1])
+        self.speedY = 7 * random.choice([-1, 1])
+        self.scoreboard = scoreboard
+        self.isPaused = isPaused
+
+    def move(self):
+        global isPaused, stopTick
+
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+
+        if self.rect.top <= 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.speedY *= -1
+        if self.rect.left <= 0:
+            self.scoreboard["opponent"] += 1
+            stopTick = pygame.time.get_ticks()
+            isPaused = True
+        if self.rect.right >= SCREEN_WIDTH:
+            self.scoreboard["player"] += 1
+            stopTick = pygame.time.get_ticks()
+            isPaused = True
+        
+        if self.rect.colliderect(player):
+            if abs(self.rect.left - player.rect.right) < 10:
+                self.speedX *= -1
+            else:
+                self.speedY *= -1
+        
+        if self.rect.colliderect(opponent):
+            if abs(self.rect.right - opponent.rect.left) < 10:
+                self.speedX *= -1
+            else:
+                self.speedY *= -1
+    
+    def restart(self):
+        self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.speedX *= random.choice([-1, 1])
+        self.speedY *= random.choice([-1, 1])
+
+class Paddle:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def move(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+class Player(Paddle):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.speed = 0
+    
+    def move(self):
+        self.rect.y += self.speed
+        super().move()
+
+class Opponent(Paddle):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.speed = 7
+    
+    def move(self):
+        if self.rect.top < ball.rect.top:
+            self.rect.top += self.speed
+        if self.rect.bottom > ball.rect.bottom:
+            self.rect.bottom -= self.speed
+        super().move()
+
+# Objects
+ball = Ball(scoreboard)
+player = Player(10 , SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
+opponent = Opponent(SCREEN_WIDTH - PLAYER_WIDTH - 10, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 
 # Game loop
 isRunning = True
@@ -135,23 +159,23 @@ while isRunning:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                player_speed -= 7
+                player.speed -= 7
             if event.key == pygame.K_DOWN:
-                player_speed += 7
+                player.speed += 7
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
-                player_speed += 7
+                player.speed += 7
             if event.key == pygame.K_DOWN:
-                player_speed -= 7
+                player.speed -= 7
 
     # Game logic
     if not isPaused:
-        ball_animation()
+        ball.move()
     else:
         countdown_timer()
 
-    player_animation()
-    opponent_animation()
+    player.move()
+    opponent.move()
 
     # Drawing
     draw()
