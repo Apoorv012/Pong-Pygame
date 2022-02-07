81c8bd15b3fd297524ecf2755a684938b7d88d94
Apoorv012 <apoorvmitthu@gmail.com>
2022-02-07 11:22:12 +0530

    Made the scores and timer works (text is PAUSED rn)

--- game.py
+++ game.py
@@ -7,10 +7,6 @@ import random
 pygame.init()
 clock = pygame.time.Clock()
 
-# Setting up the main window
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Pong Game")
-
 # CONSTANTS
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
@@ -19,20 +15,33 @@ PLAYER_HEIGHT = 140
 FPS = 60
 BALL_SIZE = 30
 
-BALL_SPEED_X = 7 * random.choice([-1, 1])
-BALL_SPEED_Y = 7 * random.choice([-1, 1])
 OPPONENT_SPEED = 7
 
 BG_COLOR = pygame.Color('grey12')
 LIGHT_GREY = (200, 200, 200)
+
+FONT_SIZE = 32
+GAME_FONT = pygame.font.Font("freesansbold.ttf", FONT_SIZE)
+PAUSE_FONT = pygame.font.Font("freesansbold.ttf", FONT_SIZE * 2)
+
+# Setting up the main window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pong Game")
 
 # Game Rectangles
 ball = pygame.Rect(SCREEN_WIDTH / 2 - BALL_SIZE / 2, SCREEN_HEIGHT / 2 - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE)
 player = pygame.Rect(10 , SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 opponent = pygame.Rect(SCREEN_WIDTH - PLAYER_WIDTH - 10, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-# Game values
+# Game variables
+ball_speed_x = 7 * random.choice([-1, 1])
+ball_speed_y = 7 * random.choice([-1, 1])
 player_speed = 0
+player_score = 0
+opponent_score = 0
+isPaused = False
+stopTick = None
+currentTick = None
 
 ## Functions
 def draw():
@@ -43,20 +52,37 @@ def draw():
     pygame.draw.rect(screen, LIGHT_GREY, opponent)
     pygame.draw.ellipse(screen, LIGHT_GREY, ball)
     pygame.draw.aaline(screen, LIGHT_GREY, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
+
+    player_score_text = GAME_FONT.render(f"{player_score}", False, LIGHT_GREY)
+    screen.blit(player_score_text, (SCREEN_WIDTH / 2 - player_score_text.get_width() / 2 - 40, SCREEN_HEIGHT / 2 - FONT_SIZE / 2))
+
+    opponent_score_text = GAME_FONT.render(f"{opponent_score}", False, LIGHT_GREY)
+    screen.blit(opponent_score_text, (SCREEN_WIDTH / 2 - opponent_score_text.get_width() / 2 + 40, SCREEN_HEIGHT / 2 - FONT_SIZE / 2))
+
+    if isPaused:
+        pause_text = PAUSE_FONT.render("PAUSED", False, LIGHT_GREY)
+        screen.blit(pause_text, (SCREEN_WIDTH / 2 - pause_text.get_width() / 2, SCREEN_HEIGHT / 2 - FONT_SIZE / 2 - 100))
+
     pygame.display.update()
 
 def ball_animation():
-    global BALL_SPEED_X, BALL_SPEED_Y
-    ball.x += BALL_SPEED_X
-    ball.y += BALL_SPEED_Y
+    global ball_speed_x, ball_speed_y, opponent_score, player_score, isPaused, stopTick
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
-        BALL_SPEED_Y *= -1
-    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
-        ball_restart()
+        ball_speed_y *= -1
+    if ball.left <= 0:
+        opponent_score += 1
+        stopTick = pygame.time.get_ticks()
+        isPaused = True
+    if ball.right >= SCREEN_WIDTH:
+        player_score += 1
+        stopTick = pygame.time.get_ticks()
+        isPaused = True
     
     if ball.colliderect(player) or ball.colliderect(opponent):
-        BALL_SPEED_X *= -1
+        ball_speed_x *= -1
 
 def player_animation():
     player.y += player_speed
@@ -66,9 +92,9 @@ def player_animation():
         player.bottom = SCREEN_HEIGHT
 
 def opponent_animation():
-    if opponent.top < ball.y:
+    if opponent.top < ball.top:
         opponent.top += OPPONENT_SPEED
-    if opponent.bottom > ball.y:
+    if opponent.bottom > ball.bottom:
         opponent.bottom -= OPPONENT_SPEED
     if opponent.top <= 0:
         opponent.top = 0
@@ -76,10 +102,17 @@ def opponent_animation():
         opponent.bottom = SCREEN_HEIGHT
 
 def ball_restart():
-    global BALL_SPEED_X, BALL_SPEED_Y
+    global ball_speed_x, ball_speed_y
     ball.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    BALL_SPEED_X *= random.choice([-1, 1])
-    BALL_SPEED_Y *= random.choice([-1, 1])
+    ball_speed_x *= random.choice([-1, 1])
+    ball_speed_y *= random.choice([-1, 1])
+
+def countdown_timer():
+    global isPaused, stopTick, currentTick
+    currentTick = pygame.time.get_ticks()
+    if currentTick - stopTick > 2000:
+        ball_restart()
+        isPaused = False
 
 # Game loop
 isRunning = True
@@ -103,7 +136,11 @@ while isRunning:
                 player_speed -= 7
 
     # Game logic
-    ball_animation()
+    if not isPaused:
+        ball_animation()
+    else:
+        countdown_timer()
+
     player_animation()
     opponent_animation()
 
